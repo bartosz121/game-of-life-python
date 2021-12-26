33561431bc12731f36590f7a59d1d4e786ebc156
bartosz121 <bmagiera121@gmail.com>
2021-12-26 19:59:39 +0100

    `cell.py` refactor
    - Remove pygame
    - Inherit from `GridObject`
    - Add slots
    - Add `draw` method used in PyQt6 paint event

--- cell.py
+++ cell.py
@@ -1,28 +1,50 @@
-from pygame import Rect
-from settings import (
-    BASIC_COLORS,
-    CELL_DEFAULT_COLOR,
-    CELL_WIDTH,
-    CELL_HEIGHT,
-    SCREEN_BACKGROUND,
-)
+from PyQt6.QtCore import QPoint
+from PyQt6.QtGui import QColor, QPainter, QBrush
+
+from grid_object import GridObject
 
 
-class Cell:
-    def __init__(self, x, y):
+class Cell(GridObject):
+    __slots__ = (
+        "_x",
+        "_y",
+        "_width",
+        "_height",
+        "_is_alive",
+        "_alive_color",
+        "_dead_color",
+        "alive_neighbours",
+        "color",
+    )
+
+    def __init__(self, x, y, width, height, alive_color, dead_color):
         self._x = x
         self._y = y
+        self._width = width
+        self._height = height
         self._is_alive = False
-        self._rect = Rect(x, y, CELL_WIDTH, CELL_HEIGHT)
+        self._alive_color = alive_color
+        self._dead_color = dead_color
         self.alive_neighbours = 0
-        self.color = CELL_DEFAULT_COLOR.rgb
+        self.color = dead_color
 
     def __repr__(self):
-        return f"Cell({self.x!r}, {self.y!r})"
+        args = (
+            str(a)
+            for a in (
+                self.x,
+                self.y,
+                self._width,
+                self._height,
+                self._alive_color,
+                self._dead_color,
+            )
+        )
+        return f"Cell({', '.join(args)})"
 
     def __str__(self):
         return (
-            f"Cell({self.x}, {self.y}, is_alive={self.is_alive},"
+            f"Cell(x={self.x}, y={self.y}, is_alive={self.is_alive},"
             f" alive_neighbours={self.alive_neighbours}, color={self.color})"
         )
 
@@ -35,18 +57,20 @@ class Cell:
         return self._y
 
     @property
-    def rect(self):
-        return self._rect
-
-    @property
     def is_alive(self):
         return self._is_alive
 
     @is_alive.setter
     def is_alive(self, state: bool):
-        # if cell is alive change color to black if not to background color
-        if state:
-            self.color = BASIC_COLORS["BLACK"]
-        else:
-            self.color = SCREEN_BACKGROUND
         self._is_alive = state
+        self._set_correct_color()
+
+    def _set_correct_color(self):
+        if self._is_alive:
+            self.color = self._alive_color
+        else:
+            self.color = self._dead_color
+
+    def draw(self, painter: QPainter):
+        painter.setBrush(QBrush(self.color))
+        painter.drawRect(self._x, self._y, self._width, self._height)
