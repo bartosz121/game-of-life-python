38ef1bf5990b47ca05d00c203fa43d186d65e8a0
bartosz121 <bmagiera121@gmail.com>
2021-12-30 19:29:07 +0100

    Add methods for reading/writing

--- game_of_life/file_manager.py
+++ game_of_life/file_manager.py
@@ -1,9 +1,11 @@
 from pathlib import Path
-from typing import Sequence
+from typing import Callable, Sequence
+
 from PyQt6.QtWidgets import QFileDialog, QWidget
 
-from reader.readers import ReaderFactory
 from grid import Grid
+from reader.readers import ReaderFactory
+from writer.writers import WriterFactory
 
 
 class FileManager:
@@ -19,10 +21,17 @@ class FileManager:
 
         return f"{s} {ext_str}"
 
-    def _get_file_path(self) -> Path:
-        path_str, _ = QFileDialog.getOpenFileName(
+    def _get_file_path(self, fn: Callable, caption: str) -> Path:
+        """
+        Return user selected file path from QFileDialog
+
+        Parameters:
+            fn: static method from 'QFileDialog' used to get path to file
+            caption: String used as caption in fn
+        """
+        path_str, _ = fn(
             parent=self.parent,
-            caption="Select a file",
+            caption=caption,
             directory=str(Path.cwd()),
             filter=self.allowed_file_extensions,
         )
@@ -31,13 +40,32 @@ class FileManager:
             # could be empty when user closed file dialog without choosing a file
             path_str = "no_file"
 
+        print(path_str)
+
         return Path(path_str)
 
+    def _get_open_file_path(self) -> Path:
+        caption = "Select a file"
+        return self._get_file_path(QFileDialog.getOpenFileName, caption)
+
+    def _get_save_file_path(self) -> Path:
+        caption = "Save as file"
+        return self._get_file_path(QFileDialog.getSaveFileName, caption)
+
     def load_file(self) -> Grid | None:
-        path = self._get_file_path()
+        path = self._get_open_file_path()
         if path.name == "no_file":
             return None
 
         format = path.suffix
         reader = ReaderFactory.get_reader(format)
         return reader.create_grid(path)
+
+    def save_file(self, data: Grid) -> None:
+        path = self._get_save_file_path()
+        if path.name == "no_file":
+            return None
+
+        format = path.suffix
+        writer = WriterFactory.get_writer(format)
+        return writer.save(path, data)
